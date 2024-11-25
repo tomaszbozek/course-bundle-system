9a316ddb8c844c12a2b457f2b68f9314280c5f4d
tomasz <tomaszbozek4@wp.pl>
2024-11-25 22:42:29 +0100

    feat: initial commit;

---
+++ src/main/java/com/cbr/course_bundle_system/CourseBundleSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cbr.course_bundle_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseBundleSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseBundleSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cbr/course_bundle_system/CourseBundleSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cbr.course_bundle_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseBundleSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
